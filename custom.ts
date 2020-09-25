63d1a661bd2d1aa52cb27e10a3b6ded2258c1708
narongporn <43835385+narongporn@users.noreply.github.com>
2020-09-25 20:01:59 +0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,83 @@
+//% weight=100 color=#0fbc11 icon=""
+namespace SDS011 {
+    /**
+     * Declare buffer and set RX, TX pin
+     */
+    //% weight=100
+    //% block="connect sensor to RX %rxpin| TX %txpin"
+    //% inlineInputMode=inline
+    export function ConnectSensor(rxpin: SerialPin, txpin: SerialPin): void {
+        serial.redirect(
+            rxpin,
+            txpin,
+            BaudRate.BaudRate9600)
+    }
+
+    /**
+     * Read data from sensor and save to buffer
+     */
+    //% weight=90
+    //% block="start character"
+    export function StartCharacter () {
+        return "" + String.fromCharCode(0xAA) + String.fromCharCode(0xC0);
+    }
+
+    /**
+     * Read 10 byte of data from sensor and save to buffer
+     */
+    //% weight=80
+    //% block="read data"
+    export function ReadData (): void {
+        bufr = serial.readBuffer(10)
+    }
+
+    /**
+     * Calculate Checksum and return true if data is valid
+     * compare only low byte
+     */
+    //% weight=70
+    //% block="checksum OK?"
+    export function CalcChecksum (): boolean {
+	let temp = 0
+        for (let index = 2; index <= 7; index++) {
+        temp += bufr[index]
+    	}
+    temp = temp & 0x0F;
+	return temp == bufr[8];
+}
+
+    /**
+     * Convert number to text with length limit
+     */
+    //% weight=60
+    //% block="convert %number | to string length %length"
+    export function NumberTostring (number: number, length: number) {
+    return ("" + number + "         ").substr(0, length)
+}
+
+
+    /**
+     * Read PM2.5 data
+     */
+    //% weight=40
+    //% block="PM 2.5"
+    export function DataPM25 () {
+	return Calc16bitNumber(3);
+}
+
+    /**
+     * Read PM10 data
+     */
+    //% weight=30
+    //% block="PM 10"
+    export function DataPM10 () {
+	return Calc16bitNumber(5);
+}
+
+let bufr = pins.createBuffer(10);
+
+function Calc16bitNumber (num: number) {
+    return bufr[num] * 256 + bufr[num - 1];
+}
+
+}
